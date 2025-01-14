7beeb608034f24bbd71930f1de49d39b06cac375
Frédéric Luce <frederic.luce@gmail.com>
2025-01-14 20:23:11 +0100

    Fix base URL

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const BASE_URL = process.env.BASE_URL || '';
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: BASE_URL,
   build: {
     target: ['chrome89', 'edge89', 'firefox89', 'safari15', 'node15'],
   }
